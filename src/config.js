46bdd0796dec3a3f85a0204bc671f03ed7f6d443
Yuna <iamrealaro@gmail.com>
2026-05-04 09:09:21 +0600

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// ─── SPOTIFY CONFIG ───────────────────────────────────────────────────────────
+export const ARTIST_ID = "51mBnXuigxNHmmUenHJpND";
+export const SPOTIFY_CLIENT_ID = "YOUR_CLIENT_ID";
+export const SPOTIFY_CLIENT_SECRET = "YOUR_CLIENT_SECRET";
