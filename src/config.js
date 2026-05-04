30c091e803893d7388cb5d7b15be1b1ab71d7dad
Yuna <iamrealaro@gmail.com>
2026-05-04 09:12:27 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // ─── SPOTIFY CONFIG ───────────────────────────────────────────────────────────
 export const ARTIST_ID = "51mBnXuigxNHmmUenHJpND";
-export const SPOTIFY_CLIENT_ID = "YOUR_CLIENT_ID";
-export const SPOTIFY_CLIENT_SECRET = "YOUR_CLIENT_SECRET";
+export const SPOTIFY_CLIENT_ID = "b518e378e27a4a9999b29958c79c6f54";
+export const SPOTIFY_CLIENT_SECRET = "4ad34a6fc4a840a9b5c31af15de66a2c";
